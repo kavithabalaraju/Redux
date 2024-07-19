af8daf6186f05a2e738ff9d69436876e5c74f183
kavitha <chkavitha324@gmail.com>
2024-07-19 10:30:37 +0530

    redux changes

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+
+ const increment = () => {
+    return {
+      type:'INCREMENT'
+    }
+  };
+
+  export  default increment;
+  const decrement = () => {
+    return {
+      type:'DECREMENT'
+    }
+  }
+  
+  export {decrement};
